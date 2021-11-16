e1d727d0ec52496835c6827ecfbc37cccf94398a
Petani Kode <dani.rahmat7398@gmail.com>
2021-11-15 21:23:32 -0800

    push update

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -1,24 +1,39 @@
 <?php
 
-
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Barang extends CI_Controller
 {
 
-    function __construct()
-    {
-        parent::__construct();
-        $this->load->model('barang_model');
-    }
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('barang_model');
+    // if (!$this->session->barangdata('auth')) {
+    //     $this->session->set_flashdata('messageAkses', 'Anda tidak berhak mengakses halaman barang');
+    //     redirect('auth');
+    // }
+  }
 
-    public function index()
-    {
-        $data['tampil_barang'] = $this->barang_model->tampilkan_barang();
-        $data['judul'] = 'Coba judul barang';
-        $data['isi'] = 'Coba isi barang';
-        $this->load->view('barang_view', $data);
-    }
+  public function index()
+  {
+    $data['barang'] = $this->barang_model->getBarang();
+    $this->load->view('barang_view', $data);
+  }
+
+  function addBarang()
+  {
+    $this->load->view('add_barang');
+  }
+
+  function create()
+  {
+    $nama = $this->input->post('nama_barang');
+    $harga = $this->input->post('harga');
+    $kategori = $this->input->post('kategori');
+    $jumlah = $this->input->post('jumlah_barang');
+    $merk = $this->input->post('merk');
+    $this->barang_model->addBarang($nama, $harga, $kategori, $jumlah, $merk);
+    redirect('barang');
+  }
 }
-
-/* End of file Barang.php */
--- application/models/Barang_model.php
+++ application/models/Barang_model.php
@@ -1,13 +1,27 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Barang_model extends CI_Model {
-
-  function tampilkan_barang(){
-    $barang = $this->db->get('tbl_barang');
-    return $barang;
+class Barang_model extends CI_Model
+{
+  function getBarang()
+  {
+    $result = $this->db->get('tbl_barang');
+    return $result;
   }
 
+  function addBarang($nama, $harga, $kategori, $jumlah, $merk)
+  {
+    $data = array(
+      'nama_barang' => $nama,
+      'harga' => $harga,
+      'kategori' => $kategori,
+      'jumlah_barang' => $jumlah,
+      'merk' => $merk
+    );
+    $this->db->insert('tbl_barang', $data);
+  }
 }
 
+
+/* End of file filename.php */
